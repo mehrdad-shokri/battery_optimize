9ec7ac7706edc806539726bd862c713018b2c43b
Mehrdad Shokri <code.poet95@gmail.com>
2020-12-26 14:37:12 +0330

    Oveeride onCreate

--- example/android/app/src/main/java/me/shokri/battery_optimize_example/MainActivity.java
+++ example/android/app/src/main/java/me/shokri/battery_optimize_example/MainActivity.java
@@ -1,6 +1,14 @@
 package me.shokri.battery_optimize_example;
 
+import android.os.Bundle;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+    }
 }
